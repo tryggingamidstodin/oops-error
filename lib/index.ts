c03e19a0ae365c38a11ae7c99bcb50529f7d9d80
olafurtorfi <olafurtorfi@gmail.com>
2018-04-10 12:41:27 +0000

    ENT-1444: Oops

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,74 @@
-import { TMUserError } from './user-error'
+import { v4 as createId } from 'uuid'
 
-export { TMUserError } from './user-error'
-export { TMSystemError } from './system-error'
+export type ErrorCategory = 'UserError' | 'SystemError' | 'ProgrammerError'
+
+export interface IOopsOptions {
+    name?: string
+    message: string
+    cause?: Error
+    category: ErrorCategory
+    context?: {}
+}
+
+export class Oops extends Error {
+    id: string
+    category: string
+    context?: {}
+    cause?: Error
+
+    constructor(options: IOopsOptions) {
+        super(options.message)
+        this.name = options.name || options.category
+        this.category = options.category
+        this.context = options.context
+        this.cause = options.cause
+        this.id = createId()
+    }
+
+    fullStack = () => {
+        let str = this.stack
+        if (this.cause) {
+            const cause: any = this.cause
+            str += '\n' + (cause.fullStack ? cause.fullStack() : cause.stack)
+        }
+        return str
+    }
+}
+
+export const getErrorCategory = (err): ErrorCategory => {
+    if (err.category) {
+        return err.category as ErrorCategory
+    }
+    return 'ProgrammerError'
+}
+
+export const programmerErrorHandler = (message: string, context?: {}) => {
+    return err => {
+        throw new Oops({
+            message,
+            category: getErrorCategory(err),
+            cause: err,
+            context,
+        })
+    }
+}
+
+export const systemErrorHandler = (message: string, context?: {}) => {
+    return err => {
+        throw new Oops({
+            message,
+            category: 'SystemError',
+            cause: err,
+            context,
+        })
+    }
+}
 
 export const assertUserInput = (input: any, message: string): void => {
     if (!Boolean(input)) {
-        throw new TMUserError(message)
+        throw new Oops({
+            message,
+            category: 'UserError',
+        })
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,21 +1,29 @@
 import { expect } from 'chai'
-import { assertUserInput, TMSystemError, TMUserError } from '../lib/index'
+import {
+    assertUserInput,
+    Oops,
+    programmerErrorHandler,
+    systemErrorHandler,
+} from '../lib/index'
 
-describe('TMSystemError error', () => {
+describe('SystemError error', () => {
     const msg = 'Something happened'
 
     it('should give correct info', () => {
         const nativeError = new Error(msg)
-        const recoverableError = new TMSystemError(msg)
+        const recoverableError = new Oops({
+            message: msg,
+            category: 'SystemError',
+        })
 
         expect(recoverableError.message).to.equal(nativeError.message)
-        expect(recoverableError.name).to.equal('TMSystemError')
-        expect(recoverableError.toString()).to.equal(`TMSystemError: ${msg}`)
+        expect(recoverableError.name).to.equal('SystemError')
+        expect(recoverableError.toString()).to.equal(`SystemError: ${msg}`)
         expect(typeof recoverableError.id).to.equal('string')
 
         // Check that the stack contains a reference to the current file.
         expect(recoverableError.stack).to.match(
-            /^TMSystemError(.|\n|\r)+test\.js:.:../
+            /^SystemError(.|\n|\r)+test\.js:.:../
         )
 
         const stackStr = err =>
@@ -30,12 +38,15 @@ describe('TMSystemError error', () => {
 describe('user error', () => {
     it('should be recoverable', () => {
         const msg = 'Something happened'
-        const userError = new TMUserError(msg)
+        const userError = new Oops({
+            message: msg,
+            category: 'UserError',
+        })
 
-        expect(userError.stack).to.match(/^TMUserError(.|\n|\r)+test\.js:..:../)
+        expect(userError.stack).to.match(/^UserError(.|\n|\r)+test\.js:..:../)
         expect(userError.message).to.equal(msg)
-        expect(userError.name).to.equal('TMUserError')
-        expect(userError.toString()).to.equal(`TMUserError: ${msg}`)
+        expect(userError.name).to.equal('UserError')
+        expect(userError.toString()).to.equal(`UserError: ${msg}`)
         expect(typeof userError.id).to.equal('string')
     })
 })
@@ -49,10 +60,10 @@ describe('assertUserInput', () => {
                 const message = `value ${falsyValue} is falsy`
                 try {
                     assertUserInput(ex, message)
-                    throw new Error('should throw TMUserError')
+                    throw new Error('should throw UserError')
                 } catch (err) {
                     expect(err.message).to.equal(message)
-                    expect(err.name).to.equal('TMUserError')
+                    expect(err.name).to.equal('UserError')
                 }
             })
         })
@@ -65,5 +76,68 @@ describe('assertUserInput', () => {
                 assertUserInput(ex, `value ${ex} is truthy`)
             })
         })
+    })
+})
+
+describe('fullstack', () => {
+    it('should have get full stack', () => {
+        try {
+            throw new Error('Lowlevel error')
+        } catch (err1) {
+            try {
+                throw new Oops({
+                    message: 'Midlevel error',
+                    category: 'SystemError',
+                    cause: err1,
+                    context: {
+                        foo: 'bar',
+                    },
+                })
+            } catch (err2) {
+                try {
+                    throw new Oops({
+                        message: 'Highlevel error',
+                        category: 'SystemError',
+                        cause: err2,
+                        context: {
+                            foo: 'bar',
+                        },
+                    })
+                } catch (err3) {
+                    const fullStack = err3.fullStack() || ''
+                    expect(fullStack).to.include('Lowlevel error')
+                    expect(fullStack).to.include('Midlevel error')
+                    expect(fullStack).to.include('Highlevel error')
+                    console.log(fullStack.split('\n'))
+                }
+            }
+        }
+    })
+})
+describe('Error handler function test', () => {
+    const badFunc = () => Promise.reject(new Error('test error'))
+    it('should have programmer error constructor function', () => {
+        return badFunc()
+            .catch(programmerErrorHandler('test message', { testId: 1234567 }))
+            .then(() => {
+                throw new Error('error handler should throw error onwards')
+            })
+            .catch(err => {
+                expect(err.context).to.deep.eq({ testId: 1234567 })
+                expect(err.message).to.eq('test message')
+                expect(err.category).to.eq('ProgrammerError')
+            })
+    })
+    it('should have system error constructor function', () => {
+        return badFunc()
+            .catch(systemErrorHandler('test message', { testId: 1234567 }))
+            .then(() => {
+                throw new Error('error handler should throw error onwards')
+            })
+            .catch(err => {
+                expect(err.context).to.deep.eq({ testId: 1234567 })
+                expect(err.message).to.eq('test message')
+                expect(err.category).to.eq('SystemError')
+            })
     })
 })
