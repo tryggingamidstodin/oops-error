f4122c53f425d5aae1eb14c1844beaf921f81f96
Guðlaugur S. Egilsson <gulli@kolibri.is>
2018-04-16 11:17:28 +0000

    Add defensive getter helper function

--- lib/util.ts
+++ lib/util.ts
@@ -5,3 +5,11 @@ export const stringifyContext = (context: {}): string => {
         return `Invalid context: "${err.message}"`
     }
 }
+
+export const defensiveGet = getter => {
+    try {
+        return getter()
+    } catch (e) {
+        return 'accessing value returned an error: ' + e
+    }
+}
