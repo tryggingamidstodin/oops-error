ac4e4c7c92dc5cf2c72817e756148753317acd8e
olafurtorfi <olafurtorfi@gmail.com>
2019-01-08 13:16:08 +0000

    HUG-219: throw newProgrammer error takes optional error object

--- lib/index.ts
+++ lib/index.ts
@@ -56,7 +56,7 @@ export class Oops extends Error {
 }
 
 export const getErrorCategory = (err): ErrorCategory => {
-    if (err.category) {
+    if (err && err.category) {
         return err.category as ErrorCategory
     }
     return constants.ProgrammerError
@@ -109,11 +109,16 @@ export const operationalOops = (message: string, context?: {}) => {
     throw newOperationalOops(message, context)
 }
 
-export const newProgrammerOops = (message: string, context?: {}) => {
+export const newProgrammerOops = (
+    message: string,
+    context?: {},
+    error?: any
+) => {
     return new Oops({
         message,
-        category: constants.ProgrammerError,
+        category: getErrorCategory(error),
         context,
+        cause: error,
     })
 }
 
